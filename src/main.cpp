cb158921430ee0d527ced74b603977f1b3cfe6c3
bigbee <silvandriessche@gmail.com>
2025-07-25 14:09:44 +0200

    added window class

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "model.h"
 #include "../include/stb_image.h"
 #include "scene.h"
+#include "windowManager.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -19,8 +20,6 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-glm::mat4 projection    = glm::mat4(1.0f);
-
 Scene* myScene;
 
 std::string execute_path;
@@ -28,68 +27,31 @@ std::string execute_path;
 int main(int argc, char* argv[])
 {
 
-    stbi_set_flip_vertically_on_load(true);
+    Window myWindow;
+    myWindow.init(SCR_WIDTH, SCR_HEIGHT);
 
     std::filesystem::path exePath = std::filesystem::canonical(argv[0]).parent_path();
     execute_path = exePath;
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glEnable(GL_DEPTH_TEST); 
 
     glm::vec3 cameraPos = glm::vec3(20.0f, 0.0f, 0.0f);  
     glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
 
     myScene = new Scene(exePath);
+    myWindow.setScene(myScene);
     myScene->cam->set_pos(cameraPos);
     myScene->cam->set_direction(cameraDirection);
 
     glm::vec3 lightPos(10.2f, 1.0f, 2.0f);
     myScene->light_pos = lightPos;
 
-    myScene->add_model(exePath/"models"/"model.obj");
+    myScene->add_model(exePath/"models"/"deck.fbx");
     myScene->set_projection(SCR_WIDTH, SCR_HEIGHT);
 
     // render loop
     // -----------
-    while (!glfwWindowShouldClose(window))
+    while (!myWindow.shouldStop())
     {
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         const float radius = 10.0f;
         float camX = sin(glfwGetTime()) * radius;
         float camZ = cos(glfwGetTime()) * radius;
@@ -107,14 +69,11 @@ int main(int argc, char* argv[])
 
         float objY = cos(2*glfwGetTime()) * 3.0f;
         myScene->models[0].setPosition(0.0f, objY, 1.0f);
-        myScene->draw();
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        myWindow.render_frame();
     }
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
 }
 
---
+++ src/windowManager.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "windowManager.h"
+#include "scene.h"
+
+Window::Window(){
+     
+}
+
+Window::~Window(){
+    glfwTerminate();
+}
+
+bool Window::init(int width, int height){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // glfw window creation
+    // --------------------
+    window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, this->framebuffer_size_callback);
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+    stbi_set_flip_vertically_on_load(true);
+    return true;
+}
+
+void Window::setScene(Scene* in_scene){
+    scene = in_scene;    
+}
+
+void Window::render_frame(){
+    // input
+    // -----
+    processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    scene->draw();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+bool Window::shouldStop(){
+    return glfwWindowShouldClose(window);
+}
+
+void Window::framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    if(scene != NULL){
+        scene->set_projection(width, height);
+    }
+    glViewport(0, 0, width, height);
+}
+
+void Window::processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
